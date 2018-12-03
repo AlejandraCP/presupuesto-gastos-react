e2cfbb299dc6cda0becbe7512ed9bff847c5d919
AlejandraCP <alejandracp710@gmail.com>
2018-12-03 15:03:07 -0500

    Updates

---
+++ precache-manifest.2f967a3bbfb3ab1b04dfc45fb36fe9db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cc6c24d8ff969b3b3f7",
+    "url": "/presupuesto-gastos-react/static/js/runtime~main.2cc6c24d.js"
+  },
+  {
+    "revision": "d70914a1e6dd503a3a7d",
+    "url": "/presupuesto-gastos-react/static/js/main.d70914a1.chunk.js"
+  },
+  {
+    "revision": "3daf17738b6a3d7c0929",
+    "url": "/presupuesto-gastos-react/static/js/1.3daf1773.chunk.js"
+  },
+  {
+    "revision": "d70914a1e6dd503a3a7d",
+    "url": "/presupuesto-gastos-react/static/css/main.6e7c48e1.chunk.css"
+  },
+  {
+    "revision": "b91ba8c31f499dbb3f14a26a5661cd59",
+    "url": "/presupuesto-gastos-react/index.html"
+  }
+];
